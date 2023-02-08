06f6c5fe1a5e0d44e14d60dd234c28d3cc4830e8
gabriel <antoniogabrielsousalira2004@gmail.com>
2023-02-08 20:13:54 -0300

    add vendedor aos prod e lista de pro c/ vende

--- produtos/views.py
+++ produtos/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 
 def listagem_produtos(request):
-    context = {
+    produtos_dos_vendedores= [{
+        "vendedor" : {"nome": "pedro"},
         "produtos" : [
         {"nome": "uva", "preco" : 2}, 
         {"nome": "melancia", "preco" : 4},
         {"nome": "banana", "preco" : 3},
-        {"nome": "ggg"}
         ]
-    }
+    },
+    {
+        "vendedor" : {"nome": "jão"},
+        "produtos" : [
+        {"nome": "mandioca", "preco" : 3}, 
+        {"nome": "melancia", "preco" : 4},
+        {"nome": "peixe", "preco" : 10},
+        ]
+    },
+    ]
+    context = {"produtos_dos_vendedores": produtos_dos_vendedores}
     return render(request, "templates/listagem_produtos.html", context)
